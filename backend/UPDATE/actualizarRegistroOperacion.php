5490037cabaa6a74aa70e6df5814a0818e2c1ee3
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2025-11-11 12:11:03 -0500

    Refactorizado al sistema de registro de operaciones, se eliminó la dependencia total de los triggers

--- backend/UPDATE/actualizarRegistroOperacion.php
+++ backend/UPDATE/actualizarRegistroOperacion.php
@@ -2,15 +2,22 @@
 
 require_once '../config/cors.php';
 require_once '../config/baseDeDatos.php';
-
+require_once '../func/obtenerMeta.php';
+$minutesPerDay = getenv('MINUTES_PER_DAY') ?: 522;
+$hoursPerDay = getenv('HOURS_PER_DAY') ?: 8.7;
+$tiempoDeMontaje = NULL;
     if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
         $datos = json_decode(file_get_contents('php://input'), true);
         $regProd_id = $datos['regProd_id'] ?? NULL;
         $ref_id = $datos['ref_id'] ?? NULL;
+        $fechaString = (new DateTime())->format('Y-m-d H:i:s'); // Fecha y hora actual
         $unidadesProducidas = $datos['unidadesProducidas'] ?? NULL;
         $horario = $datos['horario'] ?? NULL;
-
-        if ($regProd_id === NULL || $ref_id === NULL || $unidadesProducidas === NULL || $horario === NULL) { 
+        $modulo = $datos['modulo'] ?? NULL;
+        $metaPorHora = $datos['meta'] ?? NULL;
+        $recalcularMeta = $datos['recalcularMeta'] ?? false;
+        $operario = $datos['op_id'] ?? NULL;
+        if ($regProd_id === NULL || $ref_id === NULL || $unidadesProducidas === NULL || $horario === NULL || $modulo === NULL) { 
             echo json_encode([
                 "ok" => false,
                 "respuesta" => "SOLICITUD INVALIDA: ERR 402"
@@ -19,10 +26,14 @@ require_once '../config/baseDeDatos.php';
         }
         $mysqli->begin_transaction();
 
+        if ($recalcularMeta) {
+            $metaPorHora = obtenerMeta($mysqli, $operario, $ref_id, $modulo, $fechaString, $minutesPerDay, $hoursPerDay);
+        }
+        echo $metaPorHora;
         try {
-            $sql = "UPDATE registro_produccion SET ref_id = ?, unidadesProducidas = ?, horario = ? WHERE regProd_id = ?";
+            $sql = "UPDATE registro_produccion SET ref_id = ?, unidadesProducidas = ?, horario = ?, modulo = ?, MetaPorEficiencia = ? WHERE regProd_id = ?";
             $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("iiii", $ref_id, $unidadesProducidas, $horario, $regProd_id);
+            $stmt->bind_param("iiisii", $ref_id, $unidadesProducidas, $horario, $modulo, $metaPorHora, $regProd_id);
             if ($stmt->execute()) {
                 $mysqli->commit();
                 echo json_encode(["ok" => true, "respuesta" => 'El registro ha sido actualizado con exito.']);
